c1261361b0cdf04d378403919495821a9a7cbd66
Cheng <cz1612000416@163.com>
2020-05-28 13:48:42 +0800

    666

--- Leetcode_12.cpp
+++ Leetcode_12.cpp
@@ -1,31 +1,34 @@
 #include "iostream"
-#include "vector"
+#include "map"
 #include "string"
+#include "vector"
 using namespace std;
 
 string intToRoman(int num)
 {
-    map<int, char> str1 = {{1, "I"}, {5, "V"}, {10, "X"}, {50, "L"}, {100, "C"}, {500, "D"}, {1000, "M"}};
-    map<int, string> str2 = {{4, "IV"}, {9, "IX"}, {40, "XL"}, {90, "XC"}, {400, "CD"}, {900, "CM"}};
-    
+    map<int, string> str = {{1, "I"}, {5, "V"}, {10, "X"}, {50, "L"}, {100, "C"}, {500, "D"}, {1000, "M"}, {4, "IV"}, {9, "IX"}, {40, "XL"}, {90, "XC"}, {400, "CD"}, {900, "CM"}};
+
     vector<int> z = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
 
     string res;
-    while(num > 0)
+    while (num > 0)
     {
-        for(int i = z.size() - 1; i > 0; i--)
+        for (int i = z.size() - 1; i > -1; i--)
         {
-            if(num > z[i])
+            if (num >= z[i])
             {
-                res += s[z[i]];
+                res += str[z[i]];
                 num -= z[i];
+                break;
             }
         }
     }
+    return res;
 }
 
 int main()
 {
-
+    string res = intToRoman(58);
+    cout << res << endl;
     return 0;
 }
