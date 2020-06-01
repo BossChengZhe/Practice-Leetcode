77c8ef7462810f62abd0f6dbabd3bc45a7793fd2
Cheng <cz1612000416@163.com>
2020-06-01 08:54:42 +0800

    每日一题

---
+++ Leetcode_1431.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+#include "vector"
+#include "algorithm"
+using namespace std;
+
+vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
+{
+    sort(candies.begin(), candies.end(), greater<int>());
+    
+}
+
+int main()
+{
+    
+    return 0;
+}
