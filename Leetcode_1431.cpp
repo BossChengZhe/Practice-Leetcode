5475b61d3315667c5fbd69af9f214d5c1f589200
Cheng <cz1612000416@163.com>
2020-06-01 08:59:41 +0800

    6.1.每日一题

--- Leetcode_1431.cpp
+++ Leetcode_1431.cpp
@@ -1,16 +1,40 @@
+#include "algorithm"
 #include "iostream"
 #include "vector"
-#include "algorithm"
 using namespace std;
 
 vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
 {
-    sort(candies.begin(), candies.end(), greater<int>());
-    
+    vector<int> temp = candies;
+    sort(temp.begin(), temp.end(), greater<int>());
+    vector<bool> res;
+    for (int i = 0; i < candies.size(); i++)
+    {
+        if (candies[i] + extraCandies >= temp[0])
+            res.push_back(true);
+        else
+            res.push_back(false);
+    }
+    return res;
+}
+
+vector<bool> kidsWithCandies2(vector<int> &candies, int extraCandies)
+// 第二个版本，由于只需要找出最大值，所以没有必要对整个数组sort
+{
+    int max_can = candies[max_element(candies.begin(), candies.end()) - candies.begin()];
+    vector<bool> res;
+    for (int i = 0; i < candies.size(); i++)
+    {
+        if (candies[i] + extraCandies >= max_can)
+            res.push_back(true);
+        else
+            res.push_back(false);
+    }
+    return res;
 }
 
 int main()
 {
-    
+
     return 0;
 }
